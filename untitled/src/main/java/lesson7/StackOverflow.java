dca4da0634be624fb174637439f778978dd7f574
Olgatltt <olgatltt@rambler.ru>
2020-03-24 16:13:18 +0400

    lesson7clean-code

--- untitled/src/main/java/lesson7/StackOverflow.java
+++ untitled/src/main/java/lesson7/StackOverflow.java
@@ -4,7 +4,6 @@ class StackOverflow {
     public static void infiniteRecursion() {
         infiniteRecursion();
     }
-
     public static void main(String[] args) {
         infiniteRecursion();
     }
--- untitled/src/main/java/lesson7/test/MyLoader.java
+++ untitled/src/main/java/lesson7/test/MyLoader.java
@@ -8,26 +8,27 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class MyLoader extends CustomClassloader {
-    private static final String  NAMECLASS = "TestClass";
+    private static final String NAMECLASS = "TestClass";
+
     @Override
     public Class findClass(String name) {
         byte[] b = loadClassFromFile(NAMECLASS);
         return defineClass(NAMECLASS, b, 0, b.length);
     }
 
-    private byte[] loadClassFromFile(String fileName)  {
+    private byte[] loadClassFromFile(String fileName) {
         InputStream inputStream = getClass().getClassLoader().getResourceAsStream(
                 fileName.replace('.', File.separatorChar) + ".class");
         byte[] buffer;
         ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
         int nextValue;
         try {
-            while ( (nextValue = inputStream.read()) != -1 ) {
+            while ((nextValue = inputStream.read()) != -1) {
                 byteStream.write(nextValue);
             }
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("no");
+            System.out.println("File upload exception");
         }
         buffer = byteStream.toByteArray();
         return buffer;
