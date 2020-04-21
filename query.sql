2bfa6ce621a76aacf8a96b46bffcfbb15d123762
Olgatltt <olgatltt@rambler.ru>
2020-04-22 00:40:17 +0400

    fixed bugs

--- query.sql
+++ query.sql
@@ -2,7 +2,7 @@
 2. select * from products where category = '8' and price ='9.99' order by category, price  
 3. select * from products where category = '8' or category = '15'
 4. select * from products where price between 10 and 20
-5. select * from orders where orderdate between '2004-01-05' and '2004-02-05' 
+5. select * from orders where orderdate between date('2004-01-27') and date('2004-02-27')
 6. select customerid, count(customerid) from orders group by customerid
 7. select sum(totalamount) from orders group by customerid, orderdate having sum(totalamount) > 100
 8. select c.firstname, c.lastname, p.title, os.orderdate from customers as c
